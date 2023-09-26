54d5fdc2377c3824f43b4f291f2696498a2d3037
Trent1048 <trentbultsma@gmail.com>
2023-09-25 17:13:48 -0700

    create the first half of naive algorithm

--- allreduce.c
+++ allreduce.c
@@ -7,12 +7,36 @@
 #include <stdlib.h>
 #include <sys/time.h>
 
-int localSum(int count) {
+int computeLocalSum(int count) {
     int sum = 0;
     for (int i = 0; i < count; i++) {
         sum += rand() % 100;
     }
     return sum;
+}
+
+void allReduceNaive(int rank, int p, int n) {
+    const int localSum = computeLocalSum(n / p);
+    int sum = localSum;
+    int receiveBuf[1];
+    int sendBuf[1];
+
+    const int srcRank = rank - 1;
+    const int destRank = rank + 1;
+
+    if (srcRank >= 0) { // not the start
+        MPI_Status status;
+        MPI_Recv(receiveBuf, 1, MPI_INTEGER, srcRank, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+        int recievedSum = receiveBuf[0];
+        sum += recievedSum;
+    }
+    if (destRank < p) { // not the end
+        sendBuf[0] = sum;
+        MPI_Send(sendBuf, 1, MPI_INTEGER, destRank, 0, MPI_COMM_WORLD);
+    }
+
+    printf("Rank = %d: local sum = %d\n",rank,localSum);
+    printf("Rank = %d: global sum = %d\n",rank,sum);
 }
 
 int main(int argc,char *argv[]) {
@@ -22,12 +46,10 @@ int main(int argc,char *argv[]) {
     MPI_Comm_rank(MPI_COMM_WORLD,&rank);
     MPI_Comm_size(MPI_COMM_WORLD,&p);
 
-    printf("my rank = %d\n",rank);
-    printf("Rank = %d: number of processes = %d\n",rank,p);
-
+    int n = p * 10;
     srand(rank + 1);
 
-    printf("Local sum = %d\n",localSum(5));
+    allReduceNaive(rank, p, n);
 
     MPI_Finalize();
 }
